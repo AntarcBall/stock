8d5820183208be60adde054e8f43742738d59287
AntarcBall <78745248+AntarcBall@users.noreply.github.com>
2021-03-06 14:35:44 +0900

    사고 팔기, 업다운 등

--- script.js
+++ script.js
@@ -2,56 +2,86 @@ let stockList = [];
 let haveList = {}; 
 let change;
 let time= 0;
+let day = 0;
 let money = 3000;
-function intRand(a,b){return Math.floor((Math.random() * (b-a+1)) + a)};
+let seeRange = 1;
+var message= '';
+var floorY = 925
+var c = [];
+function intRand(a,b){return Math.floor((Math.random() * (b-a+1)) + a)};//기반함수
 const cvs = document.createElement('canvas');
 function appear(name,event,func){
     document.body.appendChild(name)
     name.addEventListener(event,func)
-}/////////////
+}
+function domCreate(type){return document.createElement(type);}
+///DOM요소 추가
 document.body.appendChild(cvs);cvs.style.borderStyle = 'solid';cvs.style.borderWidth = '1px'
 ctx = cvs.getContext('2d');
 document.write('<br/>')
-const nextbtn = document.createElement('button');nextbtn.textContent = 'next'
-appear(nextbtn,'click',shift)
-const input = document.createElement('input')
+const infbox = domCreate('div');//정보표시
+appear(infbox)
+const nextbtn = domCreate('button');nextbtn.textContent = 'sleep'//다음 버튼
+appear(nextbtn,'click',changeValueRandomly)
+const input = domCreate('input')//번호
 input.style.width = '30px';input.type = 'number';input.value = 0
 appear(input,'change',paint)
-const command = document.createElement('input');
-command.onkeyup = press
+const command = domCreate('input');//커맨드창
+command.onkeyup = press;command.style.width = '110px'
 appear(command)
 createStocksDisplay();
-const infbox = document.createElement('div');infbox.style.borderStyle = 'solid';infbox.style.width='200px'
-appear(infbox)
-const logg = document.createElement('div');
-logg.style.borderStyle = 'solid';logg.style.borderWidth = '1px';logg.style.width = '400px';logg.style.height = '100px';logg.style.overflow='auto'
+const logg = domCreate('div');//로그
+logg.style.borderStyle = 'solid';logg.style.borderWidth = '1px';logg.style.width = '200px';logg.style.height = '200px';logg.style.overflow='auto'
 appear(logg)
-function logger(value,br){
-    logg.innerHTML = logg.innerHTML + value + '<br/>'.repeat(br)
+////함수
+cvs.addEventListener('mousemove', function(evt){ //좌표획득
+    var mousePos = getMousePos(cvs, evt); 
+    message = `${mousePos.y}$ (time = ${mousePos.x})`
+})
+function logger(value){
+    logg.innerHTML = logg.innerHTML +value+ '<br/>'
+    console.log(value)
 }
 function press(){
-    if (window.event.keyCode == 13) {
-        coms = command.value.split(' ')
-        if ([coms[0]] == 'buy') //command
-            {if (stockList[coms[1]].slice(-1) * coms[2]<=money)
-                {haveList[coms[1]] += Number(coms[2])
-                logger(coms[1]+'번째 주를'+coms[2]+'주 '+ coms[0],1);
-                money -= stockList[coms[1]].slice(-1) * coms[2]}
-            else logger('돈 부족;',1)}
-        else if ([coms[0]] == 'show'){
-            console.log('s')
-            if ([coms[1]] == 'hands') 
-                for(i in haveList)
-                    {logger(String(i)+':'+haveList[i]+', ',0)}
+    if (window.event.keyCode == 13){
+        c = command.value.split(' ')
+        if (c[0] == 'buy') {//command
+            if (stockList[c[1]].slice(-1) * c[2]<=money){
+                haveList[c[1]] += Number(c[2])
+                logger(c[1]+'번째 주를'+c[2]+'주 '+ c[0]);
+                money -= stockList[c[1]].slice(-1) * c[2]
+            }
+            else logger('돈 부족;')
         }
-   }
+        else if (c[0] == 'show'){
+            if (c[1] == 'hands') 
+                for(i in haveList)
+                    {logger(String(i)+':'+haveList[i]+', '),false}
+        }
+        else if (c[0] == 'up'){
+            floorY -=10
+            paint();
+        }
+        else if (c[0] == 'dn'){
+            floorY +=10
+            paint();
+        }
+        
+        else if (c[0] == 'sell'){
+            if(haveList[c[1]] >= c[2] ){
+                logger(c[1]+'번째 주를'+c[2]+'주 '+ c[0])
+                money += stockList[c[1]]*c[2]
+                haveList[c[1]] -= c[2]
+            }else logger('충분한 주식을 소유하세요.')
+        }
+    }
 }
+    
 function initInf(){
-    infbox.innerHTML = `${money}$<br/>주식게임`
+    infbox.innerHTML = `It's ${message}<br/>deposit = ${money}$`
 }
 ///////////////
 function createStocksDisplay(){
-
     for (var i=0;i<10;i++){
         e = document.createElement('div')
         e.style.borderStyle = 'solid'
@@ -64,36 +94,43 @@ function createStocksDisplay(){
         stockList[i] = [1000];
     }
 }
-function shift(){
+function changeValueRandomly(){
     for(i in stockList){
         change = intRand(-10,10);
         var p = stockList[i][time] + change
         stockList[i].push(p);
-        if (time > 55) {
+        if (time > 60) {
             for (k in stockList)
                 {stockList[k].shift();}
             time --;
         }
     }
     time ++;
+    day++;
     paint();
+}
+function getMousePos(canvas, evt) { 
+    var rect = canvas.getBoundingClientRect();
+    return { 
+        x: (day-time) + Math.floor((evt.clientX - rect.left)/5),
+        y: 150 - (seeRange*(evt.clientY - rect.top ) - floorY)
+    } 
 }
 function paint(){
     what = (input.value)
     ctx.fillStyle = '#FFFFFF'
-    ctx.fillRect(0,0,cvs.width,cvs.height)
-    ctx.fillStyle = '#000000'
     var x= 0
     ctx.beginPath();
+    ctx.fillRect(0,0,cvs.width,cvs.height)
     for (i in stockList[what]){
-        ctx.lineTo(x,cvs.height-(stockList[what][i]-1000+75))
+        ctx.lineTo(x,cvs.height-(seeRange* stockList[what][i]-floorY))
         ctx.stroke();
         x+= 5;
     }
     ctx.closePath();
 }
 
-setInterval(() => {
+setInterval(() => { //값바꾸기 ;
     for( i in stockList){
         var delta = stockList[i][time] - stockList[i][time-1]
         document.getElementById(i).textContent = i +' | '+ stockList[i][time] + (delta ? (delta>0 ? '▲':'▼') + Math.abs(delta): '')
@@ -106,3 +143,4 @@ setInterval(() => {
     if( Number(input.value) > stockList.length -1) input.value = 0
     initInf();
 }, 100);
+ 
